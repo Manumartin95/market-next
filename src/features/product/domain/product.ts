363125a788ee2aa1a5c68d1e2f311c05825c2722
Manumartin95 <manuelmartinra@gmail.com>
2024-04-10 16:04:23 +0200

    feat: add prettier

--- src/features/product/domain/product.ts
+++ src/features/product/domain/product.ts
@@ -1,16 +1,21 @@
 export class Product {
-    readonly id: string
-    readonly title: string
-    readonly description: string
-    readonly category: {}
-    readonly images: string[]
+  readonly id: string
+  readonly title: string
+  readonly description: string
+  readonly category: {}
+  readonly images: string[]
 
-
-    constructor(id: string, title: string, description: string, category: {}, images: string[]) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.images = images;
-    }
+  constructor(
+    id: string,
+    title: string,
+    description: string,
+    category: {},
+    images: string[],
+  ) {
+    this.id = id
+    this.title = title
+    this.description = description
+    this.category = category
+    this.images = images
+  }
 }

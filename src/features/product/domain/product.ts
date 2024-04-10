a91bd60c7b6c676cef4bd5f65caf9eca52e2e84b
Manumartin95 <manuelmartinra@gmail.com>
2024-04-10 15:52:02 +0200

    feat: add product domain

---
+++ src/features/product/domain/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+    readonly id: string
+    readonly title: string
+    readonly description: string
+    readonly category: {}
+    readonly images: string[]
+
+
+    constructor(id: string, title: string, description: string, category: {}, images: string[]) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.images = images;
+    }
+}

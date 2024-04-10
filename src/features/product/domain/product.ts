2940246321335d0454ddaf48579564758eb49c77
Manumartin95 <manuelmartinra@gmail.com>
2024-04-10 20:09:47 +0200

    feat: update types

--- src/features/product/domain/product.ts
+++ src/features/product/domain/product.ts
@@ -2,10 +2,10 @@ export class Product {
   readonly id: number
   readonly title: string
   readonly description: string
-  readonly category: {}
+  readonly category: string
   readonly images: string[]
 
-  constructor(id: number, title: string, description: string, category: {}, images: string[]) {
+  constructor(id: number, title: string, description: string, category: string, images: string[]) {
     this.id = id
     this.title = title
     this.description = description
--- src/features/product/infrastructure/mappers/product-mapper.ts
+++ src/features/product/infrastructure/mappers/product-mapper.ts
@@ -3,7 +3,13 @@ import { Product } from '@/features/product/domain/product'
 
 export class ProductMapper {
   static toDomain(productDto: ProductDto): Product {
-    return new Product(productDto.id, productDto.title, productDto.description, productDto.category, productDto.images)
+    return new Product(
+      productDto.id,
+      productDto.title,
+      productDto.description,
+      productDto.category.name,
+      productDto.images,
+    )
   }
 
   static toDomainList(products: ProductDto[]): Product[] {

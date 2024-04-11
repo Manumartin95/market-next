8e86831056c847980fe238da5f23253287c541da
Manumartin95 <manuelmartinra@gmail.com>
2024-04-11 10:56:24 +0200

    feat: create value object for image

--- src/features/product/domain/product.ts
+++ src/features/product/domain/product.ts
@@ -1,15 +1,17 @@
+import { Image } from '@/features/product/domain/value-object/image.value-object'
+
 export class Product {
   readonly id: number
   readonly title: string
   readonly description: string
   readonly category: string
-  readonly images: string[]
+  readonly images: Image[]
 
   constructor(id: number, title: string, description: string, category: string, images: string[]) {
     this.id = id
     this.title = title
     this.description = description
     this.category = category
-    this.images = images
+    this.images = images.map(image => Image.create(image))
   }
 }
---
+++ src/features/product/domain/value-object/image.value-object.ts
@@ -0,0 +1,9 @@
+import { ValueObject } from '@/features/core/types/value-object'
+
+export class Image extends ValueObject<string> {
+  static create(value: string) {
+    const regex = new RegExp(/"(https:\/\/[^"]+)"/)
+
+    return new Image(value.match(regex)![1])
+  }
+}

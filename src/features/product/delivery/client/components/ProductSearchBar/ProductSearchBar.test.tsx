aaf7acc3a6e3bbbfb867bf08c62294d8dec74837
Manumartin95 <manuelmartinra@gmail.com>
2024-05-16 15:24:12 +0200

    test: solve search bar test

--- src/features/product/delivery/client/components/ProductSearchBar/ProductSearchBar.test.tsx
+++ src/features/product/delivery/client/components/ProductSearchBar/ProductSearchBar.test.tsx
@@ -1,12 +1,10 @@
-import {fireEvent, render, screen, waitFor} from '@testing-library/react'
-import {ProductSearchBar} from '@/features/product/delivery/client/components/ProductSearchBar/ProductSearchBar'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { ProductSearchBar } from '@/features/product/delivery/client/components/ProductSearchBar/ProductSearchBar'
 
-// Mock next/navigation
-
-const pushSpy = jest.fn()
+const push = jest.fn()
 jest.mock('next/navigation', () => ({
-  useRouter: jest.fn().mockReturnValue({
-    push: () => pushSpy,
+  useRouter: () => ({
+    push,
   }),
   useSearchParams: jest.fn().mockReturnValue(new URLSearchParams('')),
   usePathname: jest.fn().mockReturnValue('/foo-path'),
@@ -16,10 +14,6 @@ describe('ProductSearchBar', () => {
   const setup = () => {
     render(<ProductSearchBar />)
   }
-
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
 
   it('should render the input and button', () => {
     setup()
@@ -35,27 +29,29 @@ describe('ProductSearchBar', () => {
     expect(input).toHaveValue('new search term')
   })
 
-  it('should call router.push with the correct query string on form submit', () => {
+  it('should call router.push with the correct query string on form submit', async () => {
     setup()
+    // const { push } = setup()
     const input = screen.getByRole('textbox')
     const button = screen.getByRole('button', { name: /search/i })
 
     fireEvent.change(input, { target: { value: 'search term' } })
     fireEvent.click(button)
 
-    waitFor(() => {
-      expect(pushSpy).toHaveBeenCalledWith('/foo-path?search=search+term')
+    await waitFor(() => {
+      expect(push).toHaveBeenCalledWith('/foo-path?search=search+term')
     })
   })
 
-  it('should call router.push with the pathname only when input is empty on form submit', () => {
+  it('should call router.push with the pathname only when input is empty on form submit', async () => {
     setup()
+    // const { push } = setup()
     const button = screen.getByRole('button', { name: /search/i })
 
     fireEvent.click(button)
 
-    waitFor(() => {
-      expect(pushSpy).toHaveBeenCalledWith('/foo-path')
+    await waitFor(() => {
+      expect(push).toHaveBeenCalledWith('/foo-path')
     })
   })
 })
